a902080eed726adfeb61c719c8d927d1e7f4e7f2
rishabhvishwakarma17 <rv957526@gmail.com>
2026-09-19 17:55:40 +0530

    complete till the delete button

--- javascript.js
+++ javascript.js
@@ -5,22 +5,22 @@ const taskContainer = document.querySelector(".taskContainer");
 buttonTag.addEventListener("click", function () {
 
 
-    const task = inputTag.value;
-    if (task != "") {
-
-        const boxEle = document.createElement("div");
-        boxEle.classList.add("task");
-        boxEle.innerHTML = `<h2> ${task}</h2>
+    const task = inputTag.value.trim();
+    inputTag.value ="";
+    if(task.length == 0){
+        alert("Task Cannot be empty");
+        return;
+    }
+    const boxEle = document.createElement("div");
+    boxEle.classList.add("task");
+    boxEle.innerHTML = `<h2> ${task}</h2>
     <svg class="deleteIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"></path></svg>
     `;
-        console.log(boxEle);
-        const deleteButton = boxEle.querySelector(".deleteIcon");
-        deleteButton.addEventListener("click", function () {
-            taskContainer.removeChild(boxEle);
-        })
-        taskContainer.appendChild(boxEle);
-        inputTag.value = "";
-        console.dir(boxEle.innerHTML);
-    }
+    console.log(boxEle);
+    const deleteButton = boxEle.querySelector(".deleteIcon");
+    deleteButton.addEventListener("click", function () {
+        taskContainer.removeChild(boxEle);
+    })
+    taskContainer.appendChild(boxEle);
 
 })
